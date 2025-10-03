4674fb3330f139bb6df44b70b56a55aa526fc5fd
jahnpm <jahnpm@gmail.com>
2025-10-04 00:54:20 +0200

    Initial Commit

---
+++ GymTrackTests/GymTrackTests.swift
@@ -0,0 +1,17 @@
+//
+//  GymTrackTests.swift
+//  GymTrackTests
+//
+//  Created by jahnpm on 04.10.25.
+//
+
+import Testing
+@testable import GymTrack
+
+struct GymTrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
