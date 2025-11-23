9d002a2a3bf8d8c895a803a126ec79a256463dba
jahnpm <jahnpm@gmail.com>
2025-11-23 01:10:10 +0100

    Defined helper functions to calculate 1RMs

---
+++ GymTrack/Helpers.swift
@@ -0,0 +1,25 @@
+//
+//  Helpers.swift
+//  GymTrack
+//
+//  Created by jahnpm on 23.11.25.
+//
+
+enum Helpers {
+    static func oneRepMax(weight: Double, reps: Int) -> Double {
+        let clampedDelta = min(max(reps - 9, -2), 2)
+        let epleyFactor = 0.5 + Double(clampedDelta) * 0.25
+        let brzyckiFactor = 1.0 - epleyFactor
+        
+        return epleyFactor * epleyFormula(weight: weight, reps: reps) +
+               brzyckiFactor * brzyckiFormula(weight: weight, reps: reps)
+    }
+    
+    private static func brzyckiFormula(weight: Double, reps: Int) -> Double {
+        return weight / (1.0278 - 0.0278 * Double(reps))
+    }
+    
+    private static func epleyFormula(weight: Double, reps: Int) -> Double {
+        return weight * (1 + Double(reps) / 30)
+    }
+}
